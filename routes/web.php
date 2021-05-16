77fcfe59f327d892076f6251782ebd0406764f65
yra3 <64928855+yra3@users.noreply.github.com>
2021-05-16 12:55:05 +0500

    init2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PlaceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::get('/places', [PlaceController::class, 'index']);
+Route::get('/places/create', [PlaceController::class, 'create']);
+Route::post('/places', [PlaceController::class, 'store']);
+Route::get('/places/{place}', [PlaceController::class, 'show']);
+Route::get('/places/{place}/edit', [PlaceController::class, 'edit']);
+Route::put('/places/{place}', [PlaceController::class, 'update']);
+Route::patch('/places/{place}', [PlaceController::class, 'update']);
+Route::delete('/places/{place}', [PlaceController::class, 'destroy']); 
+
+require __DIR__.'/auth.php';
